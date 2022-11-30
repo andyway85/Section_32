dde5369bbaef124604cb2fef2ae8acbfe8db5d1e
Andres Camino <yourgitemail@example.com>
2022-11-30 13:57:32 -0500

    Mongoose and first type of authentication added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -12,6 +13,14 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/section32DB");
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+
+const User = mongoose.model("User", userSchema);
 
 app.get("/", function(req, res){
   res.render("home");
@@ -27,6 +36,38 @@ app.get("/register", function(req, res){
   res.render("register");
 });
 
+
+app.post("/register", function(req, res){
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+
+  newUser.save(function(err){
+    if(!err){
+      res.render("secrets");
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+
+app.post("/login", function(req, res){
+  const username = req.body.username;
+  const password = req.body.password;
+  User.findOne({email: username}, function(err, foundUser){
+    if (!err){
+      if (foundUser) {
+        if (foundUser.password === password) {
+          res.render("secrets");
+        }
+      }
+    } else {
+      console.log(err);
+    }
+  });
+});
 
 
 app.listen(3000, function() {
